dc1d1fd89d012d046ffe0e535057952eb402ae58
ZzqMa <buttlaface@gmail.com>
2025-04-03 00:39:50 -0500

    Update getassest.sh

--- getassest.sh
+++ getassest.sh
@@ -1,5 +1,5 @@
 sudo cp -R  ~/toadsi3/assets/HackNerdFont /usr/share/fonts
 cp ~/toadsi3/assets/i3catpuccin/kitty/cat.conf ~/.config/kitty/cat.conf
-cp ~/toadsi3/assets/i3gruvbox/kitty/gruv.conf ~/.config/kitty/gruv.conf
+cp ~/toadsi3/assets/i3gruvbox/kitty/gruvbox.conf ~/.config/kitty/gruv.conf
 cp ~/toadsi3/assets/i3nord/kitty/nord.conf ~/.config/kitty/nord.conf
 echo now that you got the assets cd into ~/toadsi3/themes and pick a theme and run the sh script 
