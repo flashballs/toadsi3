3acc98698641c7bc69f2e7749e8dd05e1a5cec73
ZzqMa <buttlaface@gmail.com>
2025-04-03 00:44:15 -0500

    Update catpuccin.sh

--- themes/catpuccin.sh
+++ themes/catpuccin.sh
@@ -1,5 +1,5 @@
 cd ~/toadsi3/assets/i3catpuccin/dmenu && sudo make clean install
-cp ~/toadsi3/assets/i3catpuccin/kitty.conf ~/.config/kitty/kitty.conf
+cp ~/toadsi3/assets/i3catpuccin/kitty/kitty.conf ~/.config/kitty/kitty.conf
 cp ~/toadsi3/assets/i3catpuccin/i3/config ~/.config/i3/config
 cp ~/toadsi3/assets/i3catpuccin/polybar/config.ini ~/.config/polybar/config.ini
 
