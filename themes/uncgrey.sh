ba772effd1402bab832dc3b092a2b08016159d82
ZzqMa <buttlaface@gmail.com>
2025-04-04 02:56:11 -0500

    Add files via upload

---
+++ themes/uncgrey.sh
@@ -0,0 +1,6 @@
+cd ~/toadsi3/assets/i3uncgrey/dmenu && sudo make clean install
+cp ~/toadsi3/assets/i3uncgrey/kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp ~/toadsi3/assets/i3uncgrey/i3/config ~/.config/i3/config
+cp ~/toadsi3/assets/i3uncgrey/polybar/config.ini ~/.config/polybar/config.ini
+
+echo thank you for choosing toads i3 please logout and log back in for full resault to take place
